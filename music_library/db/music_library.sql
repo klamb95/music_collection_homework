67d0d74cd0f82f5359c0040452fdfee986b123fa
klamb95 <klamb1995@gmail.com>
2021-04-20 17:10:00 +0100

    first commit

---
+++ music_library/db/music_library.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY
+    title VARCHAR(255),
+    artist_name VARCHAR(255)
+);
